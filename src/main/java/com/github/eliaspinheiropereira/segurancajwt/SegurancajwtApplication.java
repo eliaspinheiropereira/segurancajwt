3e5bb2dcc020442b051b4c5437d50296dd3313b2
Elias P. Pereira <eliaspinheiropereiraa@gmail.com>
2026-04-22 21:19:04 -0300

    iniciando projeto

---
+++ src/main/java/com/github/eliaspinheiropereira/segurancajwt/SegurancajwtApplication.java
@@ -0,0 +1,13 @@
+package com.github.eliaspinheiropereira.segurancajwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegurancajwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegurancajwtApplication.class, args);
+	}
+
+}
